8a08c6d06efb9d084ba021bf9678482775afde1d
anik.sen.vuban <anik.sen.vuban@gmail.com>
2026-05-16 12:18:16 +0600

    Add MyFirst.java with a simple greeting program

---
+++ MyFirst.java
@@ -0,0 +1,6 @@
+public class MyFirst{
+    public static void main(String[] args) {
+        System.out.print("Lokkhiiiiii,, "); //print without a newline
+        System.out.println("I Love u!"); //print with a newline
+    }
+}
