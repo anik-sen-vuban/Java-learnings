ed906b1fc054b1050ef25dbe6ba2aa745f7dd041
Anik Sen <cse2023anik@gmail.com>
2026-09-19 12:38:34 +0600

    updated

---
+++ Test_Code/MakeATestCodeJar.java
@@ -0,0 +1,11 @@
+package Test_Code;
+
+//jar cfe MakeATestCodeJar.jar Test_Code.MakeATestCodeJar .\Test_Code\MakeATestCodeJar.class
+// c -> create
+// f -> file name
+// e -> entry point (kon class er main() method execute hobe seita)
+public class MakeATestCodeJar {
+    public static void main(String[] args) {
+        System.out.println("Helllo Jar File!");
+    }
+}
